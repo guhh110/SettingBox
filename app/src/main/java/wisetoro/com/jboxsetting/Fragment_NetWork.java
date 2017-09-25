89fdcfb4435303b93e935001fc0e2ba5da43b6f6
guhh <270199345@qq.com>
2017-09-25 17:32:12 +0800

    first

---
+++ app/src/main/java/wisetoro/com/jboxsetting/Fragment_NetWork.java
@@ -0,0 +1,66 @@
+package wisetoro.com.jboxsetting;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by guhh on 2017/9/25.
+ */
+
+public class Fragment_NetWork extends Fragment {
+    private RecyclerView recyclerView;
+    private ArrayList<HashMap<String,String>> wifi_data = new ArrayList<>();
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_network,container,false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.wifi_rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new MyAdapter());
+        wifi_data.add(null);wifi_data.add(null);wifi_data.add(null);wifi_data.add(null);wifi_data.add(null);wifi_data.add(null);
+        return view;
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(Fragment_NetWork.this.getContext()).inflate(R.layout.item_wifiinfo,parent,false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return wifi_data.size();
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder{
+            private ImageView imageView;
+            private TextView wifiName_tv;
+            private TextView wifiStatus_tv;
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                imageView = (ImageView) itemView.findViewById(R.id.icon);
+                wifiName_tv = (TextView) itemView.findViewById(R.id.wifi_name_tv);
+                wifiStatus_tv = (TextView) itemView.findViewById(R.id.wifi_status_tv);
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/wisetoro/com/jboxsetting/MainActivity.java
@@ -0,0 +1,80 @@
+package wisetoro.com.jboxsetting;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.florent37.materialviewpager.MaterialViewPager;
+import com.github.florent37.materialviewpager.header.HeaderDesign;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private MaterialViewPager mViewPager;
+    private List<Fragment> fragments;
+    private String[] titles = new String[]{"One","Two","Three","Four"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+
+    private void init(){
+        fragments = new ArrayList<>();
+        fragments.add(new Fragment_NetWork());
+        fragments.add(new Fragment_NetWork());
+        fragments.add(new Fragment_NetWork());
+        fragments.add(new Fragment_NetWork());
+
+        mViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
+        mViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return titles[position];
+            }
+        });
+
+        mViewPager.setMaterialViewPagerListener(new MaterialViewPager.Listener() {
+            @Override
+            public HeaderDesign getHeaderDesign(int page) {
+                switch (page) {
+                    case 0:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.green,
+                                "http://phandroid.s3.amazonaws.com/wp-content/uploads/2014/06/android_google_moutain_google_now_1920x1080_wallpaper_Wallpaper-HD_2560x1600_www.paperhi.com_-640x400.jpg");
+                    case 1:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.blue,
+                                "http://www.hdiphonewallpapers.us/phone-wallpapers/540x960-1/540x960-mobile-wallpapers-hd-2218x5ox3.jpg");
+                    case 2:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.cyan,
+                                "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
+                    case 3:
+                        return HeaderDesign.fromColorResAndUrl(
+                                R.color.red,
+                                "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg");
+                }
+
+                return null;
+            }
+        });
+
+        mViewPager.getViewPager().setOffscreenPageLimit(mViewPager.getViewPager().getAdapter().getCount());
+        mViewPager.getPagerTitleStrip().setViewPager(mViewPager.getViewPager());
+    }
+}
